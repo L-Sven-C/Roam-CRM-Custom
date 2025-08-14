9ad3a20fb0992c8f69d838533521dc94cf9259c3
Matt Vogel <mainmoniker@googlemail.com>
2025-08-13 22:00:41 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ import { moveFocus, getLastBlockAndFocus } from './utils';
 import EventKeywordSettings from "./components/event_keyword_settings"
 
 const testing = false
-const version = "v2.9.7"
+const version = "v2.9.8"
 
 const plugin_title = "Roam CRM"
 
